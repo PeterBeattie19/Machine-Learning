5825aba65f853dd3f04d2b9c27c2171524825652
Peter Beattie <peterbeattie19@gmail.com>
2017-11-05 12:57:10 +0000

    Create classifier.py

---
+++ IrisFlowerClassifierSVM/classifier.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 12:24:43 2017
+
+@author: peter
+"""
+
+from sklearn import datasets
+from sklearn import svm
+
+iris = datasets.load_iris()
+features = iris.data[:, :2]  # we only take the first two features.
+labels = iris.target
+
+num = int(0.66 * len(features)) 
+X = features[:num] 
+Y = labels[:num] 
+
+clf = svm.LinearSVC()
+clf.fit(X,Y) 
+
+testFeatures = features[num:]
+testLabels = labels[num:] 
+
+pred = clf.predict(testFeatures) 
+
